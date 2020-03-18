73f1cc2bebd0d4577e314f4c253fd7b12ef4ae26
EviNikolaidou <evi.nikolaidou@outlook.com>
2020-03-18 10:36:08 +0000

    logout controller

--- TravelAgenta/app/controllers/logout.scala
+++ TravelAgenta/app/controllers/logout.scala
@@ -1,1 +1,20 @@
 package controllers
+
+import akka.stream.Materializer
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Controller}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.concurrent.Future
+
+class logout @Inject()(val messagesApi: MessagesApi, val materializer: Materializer) extends Controller
+  with I18nSupport {
+
+  def logout: Action[AnyContent] = Action.async { implicit request =>
+    Future{
+      Redirect(routes.Application.index()).withSession()
+    }
+  }
+
+}
